ea15005243a1d9741cd7cbc0f181783154fdaa1a
Salman <apple@Apple-no-MacBook-Pro.local>
2019-11-15 00:09:34 +0600

    add user in movies serializer

--- app/serializers/movie_serializer.rb
+++ app/serializers/movie_serializer.rb
@@ -1,3 +1,13 @@
 class MovieSerializer < ActiveModel::Serializer
-  attributes :id, :title, :text
+  attributes :id, :title, :text, :created_at, :owner
+  belongs_to :category
+  belongs_to :user
+
+  def created_at
+  	object.created_at.strftime('%F')  	
+  end
+
+  def owner
+  	(object.user.first_name.to_s.first.upcase || '') + (object.user.last_name.to_s.first.upcase || '')
+  end
 end
